e4d67ac13c0a3a0a99b2b678868f7657ecf992dd
AIMÊ <aimemartins@hotmail.com>
2023-01-31 22:17:16 -0300

    Requisito 4

---
+++ desafio4.sql
@@ -0,0 +1,11 @@
+SELECT
+    u.usuario_nome AS usuario,
+    IF(
+        MAX(YEAR(h.data_reproducao)) >= 2021,
+        'Usuário ativo',
+        'Usuário inativo'
+    ) AS status_usuario
+FROM SpotifyClone.usuario AS u
+    INNER JOIN SpotifyClone.historico AS h ON u.usuario_id = h.usuario_id
+GROUP BY u.usuario_id
+ORDER BY u.usuario_nome;
